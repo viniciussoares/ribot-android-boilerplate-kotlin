df29ece09155f0e9b6b174cb09609434cf329a29
Vinícius Soares <squimer@gmail.com>
2016-09-24 12:45:44 -0300

    First commit

---
+++ app/src/main/kotlin/uk/co/ribot/androidboilerplate/BoilerplateApplication.kt
@@ -0,0 +1,10 @@
+package uk.co.ribot.androidboilerplate
+
+import android.app.Application
+
+class BoilerplateApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/kotlin/uk/co/ribot/androidboilerplate/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package uk.co.ribot.androidboilerplate.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import uk.co.ribot.androidboilerplate.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
