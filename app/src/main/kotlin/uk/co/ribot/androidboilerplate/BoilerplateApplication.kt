79aeee21cb637c82763a2ae54750ed73a9653aee
Vinícius Soares <squimer@gmail.com>
2016-09-24 12:49:31 -0300

    Add Dagger, Support Library, ButterKnife, RxJava and Timber.

--- app/src/main/kotlin/uk/co/ribot/androidboilerplate/BoilerplateApplication.kt
+++ app/src/main/kotlin/uk/co/ribot/androidboilerplate/BoilerplateApplication.kt
@@ -1,10 +1,15 @@
 package uk.co.ribot.androidboilerplate
 
 import android.app.Application
+import timber.log.Timber
 
 class BoilerplateApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
