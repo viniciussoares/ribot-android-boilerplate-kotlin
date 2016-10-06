703cfbe22e2c24e3ffca9a7e03192894762a8935
Vinícius Soares <squimer@gmail.com>
2016-10-05 21:34:39 -0300

    Add toolbar, recyclerview and theme tweaks.

--- app/src/main/kotlin/uk/co/ribot/androidboilerplate/ui/main/MainActivity.kt
+++ app/src/main/kotlin/uk/co/ribot/androidboilerplate/ui/main/MainActivity.kt
@@ -1,6 +1,11 @@
 package uk.co.ribot.androidboilerplate.ui.main
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import butterknife.BindView
+import butterknife.ButterKnife
 import uk.co.ribot.androidboilerplate.R
 import uk.co.ribot.androidboilerplate.data.SyncService
 import uk.co.ribot.androidboilerplate.ui.base.BaseActivity
@@ -10,11 +15,24 @@ class MainActivity : BaseActivity() {
     val EXTRA_TRIGGER_SYNC_FLAG =
             "uk.co.ribot.androidboilerplate.ui.main.MainActivity.EXTRA_TRIGGER_SYNC_FLAG"
 
+    @BindView(R.id.toolbar)
+    lateinit var toolbar: Toolbar
+
+    @BindView(R.id.recycler_view)
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        activityComponent.inject(this)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
 
-        if (intent.getBooleanExtra(EXTRA_TRIGGER_SYNC_FLAG, true))
+        setSupportActionBar(toolbar)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        if (intent.getBooleanExtra(EXTRA_TRIGGER_SYNC_FLAG, true)) {
             startService(SyncService.getStartIntent(this))
+        }
     }
 }
