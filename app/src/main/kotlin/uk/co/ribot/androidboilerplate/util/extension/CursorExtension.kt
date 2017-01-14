51ccb9d55d95097a04b5dbf1db3344d000e447d7
Vinícius Soares <squimer@gmail.com>
2017-01-13 23:04:58 -0200

    Add default values to prevent exceptions while parsing a cursor.

--- app/src/main/kotlin/uk/co/ribot/androidboilerplate/util/extension/CursorExtension.kt
+++ app/src/main/kotlin/uk/co/ribot/androidboilerplate/util/extension/CursorExtension.kt
@@ -2,18 +2,22 @@ package uk.co.ribot.androidboilerplate.util.extension
 
 import android.database.Cursor
 
-fun Cursor.getString(columnName: String): String {
-    return getString(getColumnIndexOrThrow(columnName))
+fun Cursor.getString(columnName: String, defaultValue: String = ""): String {
+    val index = getColumnIndex(columnName)
+    return getString(index) ?: defaultValue
 }
 
-fun Cursor.getInt(columnName: String): Int {
-    return getInt(getColumnIndexOrThrow(columnName))
+fun Cursor.getInt(columnName: String, defaultValue: Int = 0): Int {
+    val index = getColumnIndex(columnName)
+    return if (index >= 0) getInt(index) else defaultValue
 }
 
-fun Cursor.getLong(columnName: String): Long {
-    return getLong(getColumnIndexOrThrow(columnName))
+fun Cursor.getLong(columnName: String, defaultValue: Long = 0): Long {
+    val index = getColumnIndex(columnName)
+    return if (index >= 0) getLong(index) else defaultValue
 }
 
-fun Cursor.getBoolean(columnName: String): Boolean {
-    return getInt(columnName) == 1
+fun Cursor.getBoolean(columnName: String, defaultValue: Boolean = false): Boolean {
+    val index = getColumnIndex(columnName)
+    return if (index >= 0) getInt(index) == 1 else defaultValue
 }
