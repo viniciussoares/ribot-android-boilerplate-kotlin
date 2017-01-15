41dd365e417cb9f933ae51109d55bc5eb145f017
Vinícius Soares <squimer@gmail.com>
2017-01-14 23:28:37 -0200

    Placeholder

---
+++ app/src/androidTest/java/uk/co/ribot/androidboilerplate/runner/RxAndroidJUnitRunner.java
@@ -0,0 +1,8 @@
+package uk.co.ribot.androidboilerplate.runner;
+
+/**
+ * Created by vinicius on 13/01/17.
+ */
+
+public class RxAndroidJUnitRunner {
+}
---
+++ app/src/androidTest/java/uk/co/ribot/androidboilerplate/util/RxIdlingResource.java
@@ -0,0 +1,8 @@
+package uk.co.ribot.androidboilerplate.util;
+
+/**
+ * Created by vinicius on 13/01/17.
+ */
+
+public class RxIdlingResource {
+}
