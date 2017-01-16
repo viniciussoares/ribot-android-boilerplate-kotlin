cf1b048f40ffcecdf85fbc76dcf3e008a6a3d44c
Vinícius Soares <squimer@gmail.com>
2017-01-15 22:40:27 -0200

    Add espresso, test runner and rules.

--- app/src/androidTest/java/uk/co/ribot/androidboilerplate/runner/UnlockDeviceAndroidJUnitRunner.kt
+++ app/src/androidTest/java/uk/co/ribot/androidboilerplate/runner/UnlockDeviceAndroidJUnitRunner.kt
@@ -1,6 +1,5 @@
 package uk.co.ribot.androidboilerplate.runner
 
-import android.annotation.SuppressLint
 import android.app.Application
 import android.app.KeyguardManager
 import android.os.PowerManager
@@ -16,27 +15,29 @@ import android.os.PowerManager.ON_AFTER_RELEASE
  * Extension of AndroidJUnitRunner that adds some functionality to unblock the device screen
  * before starting the tests.
  */
-class UnlockDeviceAndroidJUnitRunner : AndroidJUnitRunner() {
+open class UnlockDeviceAndroidJUnitRunner : AndroidJUnitRunner() {
 
-    private var mWakeLock: PowerManager.WakeLock? = null
+    private lateinit var mWakeLock: PowerManager.WakeLock
 
     override fun onStart() {
         val application = targetContext.applicationContext as Application
         val simpleName = UnlockDeviceAndroidJUnitRunner::class.java.simpleName
+
         // Unlock the device so that the tests can input keystrokes.
         (application.getSystemService(KEYGUARD_SERVICE) as KeyguardManager)
                 .newKeyguardLock(simpleName)
                 .disableKeyguard()
+
         // Wake up the screen.
         val powerManager = application.getSystemService(POWER_SERVICE) as PowerManager
         mWakeLock = powerManager.newWakeLock(FULL_WAKE_LOCK or ACQUIRE_CAUSES_WAKEUP or
                 ON_AFTER_RELEASE, simpleName)
-        mWakeLock!!.acquire()
+        mWakeLock.acquire()
         super.onStart()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mWakeLock!!.release()
+        mWakeLock.release()
     }
 }
